e7b633cb0a1b0110e78c9d3d35ce3c9d3a924e34
Matias Solis <solisma42@gmail.com>
2022-03-31 16:02:47 -0300

    Added Projects videos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import image from '../images/placeholder.png'
+import league from "../videos/league.webm"
+import pet from "../videos/pet.webm"
 
 const Projects = () => {
 	return (
@@ -7,59 +8,37 @@ const Projects = () => {
 			<div className='projects__title'>SELECTED PROJECTS</div>
 			<div className='projects__container'>
 				<div className='projects__item'>
-					<img className='projects__video' src={image} alt="poject" />
+					<video muted autoPlay loop className='projects__video'>
+						<source src={league} type="video/webm" />
+					</video>
 					<div className='projects__info'>
 						<p className='projects__name'>League Maker</p>
 						<ul className='projects__list'>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
+							<li className='projects__tech'>react</li>
+							<li className='projects__tech'>react router</li>
+							<li className='projects__tech'>axios</li>
+							<li className='projects__tech'>formik</li>
+							<li className='projects__tech'>yup</li>
+							<li className='projects__tech'>css</li>
 						</ul>
 						<p className='projects_description'>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo arcu, maximus sit amet bibendum id, sollicitudin vel dolor. Ut et dapibus erat.
 						</p>
+						{/* <i className="fa-brands fa-github" />
+						<p>live</p> */}
 					</div>
 				</div>
 				<div className='projects__item'>
-					<img className='projects__video' src={image} alt="poject" />
+					<video muted autoPlay loop className='projects__video'>
+						<source src={pet} type="video/webm" />
+					</video>
 					<div className='projects__info'>
-						<p className='projects__name'>League Maker</p>
+						<p className='projects__name'>Family Pets</p>
 						<ul className='projects__list'>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-						</ul>
-						<p className='projects_description'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo arcu, maximus sit amet bibendum id, sollicitudin vel dolor. Ut et dapibus erat.
-						</p>
-					</div>
-				</div>
-				<div className='projects__item'>
-					<img className='projects__video' src={image} alt="poject" />
-					<div className='projects__info'>
-						<p className='projects__name'>League Maker</p>
-						<ul className='projects__list'>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-						</ul>
-						<p className='projects_description'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo arcu, maximus sit amet bibendum id, sollicitudin vel dolor. Ut et dapibus erat.
-						</p>
-					</div>
-				</div>
-				<div className='projects__item'>
-					<img className='projects__video' src={image} alt="poject" />
-					<div className='projects__info'>
-						<p className='projects__name'>League Maker</p>
-						<ul className='projects__list'>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
-							<li className='projects__tech'>lorem</li>
+							<li className='projects__tech'>react</li>
+							<li className='projects__tech'>css</li>
+							<li className='projects__tech'>formik</li>
+							<li className='projects__tech'>firebase</li>
 						</ul>
 						<p className='projects_description'>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo arcu, maximus sit amet bibendum id, sollicitudin vel dolor. Ut et dapibus erat.
